8165a25b63f065e880f3814816ba75c8887ebf14
4lex4c <44lex.dev@gmail.com>
2024-06-09 23:51:59 -0300

    Agregué los ejemplos de variables en JavaScript

---
+++ variables/ejemplos.js
@@ -0,0 +1,35 @@
+// Ejemplos de Variables en JavaScript
+
+// Declaración de Variables
+const nombre = "Alexander";
+const apellido = "Acosta";
+let edad = 17;
+var ciudad = "Buenos Aires, Argentina";
+
+// Mostrar las variables por consola
+console.log("Nombre:", nombre); // → Alexander
+console.log("Apellido:", apellido); // → Acosta
+console.log("Edad:", edad); // → 17
+console.log("Ciudad:", ciudad); // → Buenos Aires, Argentina
+
+// Modificar variables
+edad = 18;
+ciudad = "Nueva York, Estados Unidos"
+
+// Mostrar las variables modificadas
+console.log("Edad:", edad); // → 18
+console.log("Ciudad:", ciudad); // → Nueva York, Estados Unidos
+
+// Declaración de variables con diferentes tipos de datos
+let precio = 30.50;
+let cantidad = 12;
+let disponible = true;
+
+// Mostrar las variables con diferentes tipos de datos
+console.log("Precio:", precio);
+console.log("Cantidad:", cantidad);
+console.log("Disponibilidad:", disponible);
+
+// Concatenación de Strings (cadena; tipo de dato)
+let mensaje = "El Producto tiene un precio de $" + precio + ".";
+console.log(mensaje); // → El Producto tiene un precio de $30.5.
