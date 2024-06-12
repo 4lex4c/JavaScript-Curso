4121f2720887b735db0c11c4536a69ae010d1036
4lex4c <44lex.dev@gmail.com>
2024-06-12 12:12:43 -0300

    Sección sobre los Tipos de Datos en JavaScript

---
+++ tipos-de-datos/datos-simples.js
@@ -0,0 +1,18 @@
+// Ejemplos de Tipos de Datos Simples en JavaScript
+
+// Números
+let edad = 17;
+let currentYear = 2024;
+let memoriaRam = 8;
+
+console.log(edad + ` "${typeof edad}"`); // -> 17 "number"
+console.log(currentYear + ` "${typeof currentYear}"`); // -> 2024 "number"
+console.log(memoriaRam + ` "${typeof memoriaRam}"`); // -> 8 "number"
+
+// BigInt
+const previouslyMaxSafeInteger = 9007199254740991n;
+const hugeHex = BigInt("0x1fffffffffffff");
+const hugeString = BigInt("9007199254740991");
+
+
+
