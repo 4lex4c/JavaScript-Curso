2d6828c264dcef879bac0ca92015bbbc757b1841
4lex4c <44lex.dev@gmail.com>
2024-06-14 09:06:42 -0300

    Ciclo Do-While; Ejemplo y Explicación

---
+++ bucles-e-iteración/do-while.js
@@ -0,0 +1,14 @@
+// Declaración de Variable
+let numero = 0;
+
+// El Do While ejecuta el código y luego pregunta si se cumple la condición para seguir ejecutándolo
+// Si la condición da false, el código continua el flujo normal del código principal
+
+// Ejecuta lo que hay adentro de Do
+do {
+    console.log(`El número es ${numero}`);
+    numero++;
+}
+
+// Condición
+while(numero <= 5);
