a44666886e81cfadae43a2d83974cde237854378
4lex4c <44lex.dev@gmail.com>
2024-06-13 22:22:48 -0300

    Documentación de los diferentes bucles e iteraciones en JavaScript

---
+++ bucles-e-iteración/while.js
@@ -0,0 +1,8 @@
+let numero = 0;
+
+// El While pregunta hasta que la condición sea falsa, de lo contrario no seguirá con el flujo normal del código
+while (numero < 5) {
+    numero++;
+    console.log(numero); // 12345
+};
+
